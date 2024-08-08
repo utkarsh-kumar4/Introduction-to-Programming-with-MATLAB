4bf113e827e104cc51fe76c5b5cab1c0cc6ce9ad
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-08-08 07:04:53 +0530

    Update char_counter.m

--- Module 9/char_counter.m
+++ Module 9/char_counter.m
@@ -1,18 +1,13 @@
 function charnum = char_counter(fname,character)
     fid = fopen(fname,'rt');
-    if (fid<0 || ~ischar(character) || size(charater,2)>1)
+    if (fid<0 || ~ischar(character))
         charnum = -1;
         return;
     end
     line = fgets(fid);
     count = 0;
     while ischar(line)
-        n = size(line,2);
-        for i = 1:n
-            if (line(i)==character)
-                count = count+1;
-            end
-        end
+        count = count + sum(line == character);
         line = fgets(fid);
     end
     fclose(fid);
