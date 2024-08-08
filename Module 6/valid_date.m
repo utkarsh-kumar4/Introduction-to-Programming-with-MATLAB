fc8a0131138083de687fc060273d65e6f9ca0fb4
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-08-08 06:08:58 +0530

    Update valid_date.m

--- Module 6/valid_date.m
+++ Module 6/valid_date.m
@@ -1,23 +1,27 @@
 function valid = valid_date(year,month,date)
     flag = true;
-    if (year<=0 || month<=0 || date<=0)
+    if (~isscalar(year) || ~isscalar(month) || ~isscalar(date))
         flag = false;
     else
-        if (month>12 || date>31)
+        if (year<=0 || month<=0 || date<=0)
             flag = false;
         else
-            if (month==4 || month==6 || month==9 || month==11)
-                if (date>30)
-                    flag = false;
-                end
-            elseif (month==2)
-                if (leap_year(year))
-                    if (date>29)
+            if (month>12 || date>31)
+                flag = false;
+            else
+                if (month==4 || month==6 || month==9 || month==11)
+                    if (date>30)
                         flag = false;
                     end
-                else
-                    if (date>28)
-                        flag = false;
+                elseif (month==2)
+                    if (leap_year(year))
+                        if (date>29)
+                            flag = false;
+                        end
+                    else
+                        if (date>28)
+                            flag = false;
+                        end
                     end
                 end
             end
