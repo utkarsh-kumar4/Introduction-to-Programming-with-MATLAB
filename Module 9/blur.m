9d2b3f61ade2b97c18fb9e1e22edff984bc7396c
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-08-08 06:57:08 +0530

    Update blur.m

--- Module 9/blur.m
+++ Module 9/blur.m
@@ -1,13 +1,34 @@
-function [output] = blur(A, w)
-    [rows, columns] = size(A);
-    B = nan(size(A) + 2 * w);
-    B(w + 1 : end - w, w + 1 : end - w) = A;
-    output = 0 * A;
-    
-    for i = w + 1 : rows + w
-      for j = w + 1 : columns + w
-        tmp = B(i - w : i + w, j - w : j + w);
-        output(i - w, j - w) = mean(tmp(~isnan(tmp)));
-      end
+function out = blur(img,w)
+    % convert to double for doing calculations
+    imgD = double(img);
+    [row, col] = size(img);
+    out = zeros(row, col);
+    for ii = 1:row
+        for jj = 1:col
+            % Get the indices for a submatrix
+            r1 = ii-w;
+            r2 = ii+w;
+            c1 = jj-w;
+            c2 = jj+w;
+            % Test that indices are valid
+            % If not, set to min/max that is valid
+            if r1 < 1
+                r1 = 1;
+            end
+            if r2 > row
+                r2 = row;
+            end
+            if c1 < 1
+                c1 = 1;
+            end
+            if c2 > col
+                c2 = col;
+            end
+            % Get the submatrix and assign the mean to the output pixel
+            m = imgD(r1:r2, c1:c2);
+            out(ii,jj) = mean(m(:));
+        end
     end
+    % convert back to uint8
+    out = uint8(out);
 end
