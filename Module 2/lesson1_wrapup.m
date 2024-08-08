554fbec3d209ee69a04ece65c71cc7acc79f80d9
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-08-08 05:44:27 +0530

    Update lesson1_wrapup.m

--- Module 2/lesson1_wrapup.m
+++ Module 2/lesson1_wrapup.m
@@ -2,8 +2,10 @@ distance_100 = 100;
 time = 9.58;
 usain_mps = distance_100/time;
 hundred = usain_mps*(18/5);
+disp(hundred);
 
 distance_marathon = 42.195 * 1000;
 time = (2*3600)+(1*60)+(39);
 kipchoge_mps = distance_marathon/time;
 marathon = kipchoge_mps*(18/5);
+disp(marathon);
